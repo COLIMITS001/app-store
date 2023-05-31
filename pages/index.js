f76fcc58278a2d8b18e9aa402dd1760ec8830428
COLIMITS001 <glimitslg@gmail.com>
2023-05-31 20:12:50 +0300

    Saving on git

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
     <div className="products-heading">
-      <h2>Best Seller Products</h2>
-      <p>shoe There are many variations passages</p>
+      <h2>Urban Street Sneakers</h2>
+      <p>Indulge in the ultimate comfort and style with our timeless shoes</p>
     </div>
 
     <div className="products-container">
